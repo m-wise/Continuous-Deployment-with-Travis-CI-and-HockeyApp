7f0a3174ac1ff37bae0436973a2858219b6b3dab
Conrad Bartels Daal <cbartelsdaal@m-wise.nl>
2015-07-16 17:48:34 +0200

    added test cocoapods

---
+++ TestProject/Pods/ContentfulDeliveryAPI/Code/CDASpace.m
@@ -0,0 +1,48 @@
+//
+//  CDASpace.m
+//  ContentfulSDK
+//
+//  Created by Boris Bügling on 06/03/14.
+//
+//
+
+#import "CDAResource+Private.h"
+#import "CDASpace+Private.h"
+
+@interface CDASpace ()
+
+@property (nonatomic) NSString* name;
+
+@end
+
+#pragma mark -
+
+@implementation CDASpace
+
++(NSString *)CDAType {
+    return @"Space";
+}
+
+#pragma mark -
+
+-(id)initWithDictionary:(NSDictionary *)dictionary client:(CDAClient*)client {
+    self = [super initWithDictionary:dictionary client:client];
+    if (self) {
+        self.defaultLocale = @"en-US";
+        self.locales = dictionary[@"locales"];
+        self.name = dictionary[@"name"];
+        
+        for (NSDictionary* locale in self.locales) {
+            if ([locale[@"default"] boolValue]) {
+                self.defaultLocale = locale[@"code"];
+            }
+        }
+    }
+    return self;
+}
+
+-(NSArray *)localeCodes {
+    return [self.locales valueForKey:@"code"] ?: @[ self.defaultLocale ];
+}
+
+@end
